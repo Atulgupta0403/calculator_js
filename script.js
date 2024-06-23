bd9fea9ffa141da5755325650a747dd090a40975
atulgupta0403 <atulgupta0403@gmail.com>
2024-06-23 17:10:24 +0530

    updating multi and divide

--- script.js
+++ script.js
@@ -25,58 +25,137 @@ buttons.forEach((button)=>{
 })
 
 equal.addEventListener("click",()=>{
-    const data = output.textContent
+    let data = output.textContent
     const find = data.includes(add)
 
-    let a = ""
     let result = 0
-    for(const i in data){
-        // console.log(i)
-        let eachData = data[i]
-        // console.log(eachData)
+    let value = data.split(add)
+    console.log(typeof(value) , "value" , value)
+
+    value.map((e,i)=>{
+        
+        console.log(typeof(e),e)
+
+        // console.log("SUBTRACT WALA")
+
+        // if(e.includes(sub)){
+        //     let subtract = 0
+        //     // console.log(e)
+        //     let subt = e.split(sub)
+        //     // result = e[i]
+        //     console.log(typeof(subt), subt)
+        //     subt.map((elem , i)=>{
+        //         // console.log(elem[i],i,elem)
+        //         subtract = elem[i]
+        //         result = result + Number(elem) 
+        //     })
+
+        // }
+
+        console.log("MULTIPLY WALA")
+        
+        if(e.includes(multi)){
+
+            let multiply = 1
+
+            // console.log(e)
+            let multip = e.split(multi)
+            // result = e[i]
+            console.log(typeof(multip), multip)
+            multip.map((elem , i)=>{
+
+                multiply = multiply*Number(elem)
+                console.log(typeof(multiply) , "multiply is =" , multiply)
+
+                // console.log(elem[i],i,elem)
+            })
+            result += multiply
+            
+        }
+
+        console.log("DIVIDE WALA")
+        
+        if(e.includes(divide)){
+
+            let div = 1
+            // console.log(e)
+            let divides = e.split(divide)
+            // result = e[i]
+            console.log(typeof(divides), divides)
+            divides.map((elem , i)=>{
+
+                div = div/Number(elem)
+                console.log(div)
+                
+                
+                console.log(elem[i],i,elem)
+            })
+            result += div
+
+        }
+
+    })
+
+    // data = result
+    // console.log(`data ${data}`)
+    // value = data.split(sub)
+
+    // console.log(value)
 
 
-        if(eachData === "1" || eachData === "2" || eachData == "3" ){
-            a =a+ eachData
-            console.log("a",a)
-            // console.log("shi h")
-            // result = a
-            
-        }
-        else if (eachData === add) {
-            let num1 = Number(a)
-            console.log("num1",num1)
-            result = Number(result)
-            result += num1
-            console.log("result",result)
-            console.log(typeof(result))
+    console.log("result is = ", result)
+    output.innerHTML = result
 
-            console.log("add",eachData)
-        }
-        else if (eachData === sub) {
-            console.log("sub",eachData)
+
+    // let a = ""
+    // let result = 0
+    // for(const i in data){
+    //     // console.log(i)
+    //     let eachData = data[i]
+    //     // console.log(eachData)
+
+
+    //     if(eachData === "1" || eachData === "2" || eachData == "3" ){
+    //         a =a+ eachData
+    //         console.log("a",a)
+    //         // console.log("shi h")
+    //         // result = a
             
-        }
-        else if (eachData === multi) {
-            console.log("multiply",eachData)
+    //     }
+    //     else if (eachData === add) {
+    //         let num1 = Number(a)
+    //         console.log("num1",num1)
+    //         result = Number(result)
+    //         result += num1
+    //         console.log("result",result)
+    //         console.log(typeof(result))
+
+    //         console.log("add",eachData)
+    //     }
+    //     else if (eachData === sub) {
+    //         console.log("sub",eachData)
             
-        }
-        else if (eachData === divide) {
-            console.log("divide",eachData)
+    //     }
+    //     else if (eachData === multi) {
+    //         console.log("multiply",eachData)
             
-        }
-        else if (eachData === percentage) {
-            console.log("percentage",eachData)
+    //     }
+    //     else if (eachData === divide) {
+    //         console.log("divide",eachData)
             
-        }
-        else{
-            const sign = eachData
-            // const output 
-            // console.log(typeof())
-            // console.log("glt h")
-        }
-        output.innerHTML = result
-    }
+    //     }
+    //     else if (eachData === percentage) {
+    //         console.log("percentage",eachData)
+            
+    //     }
+    //     else{
+    //         const sign = eachData
+    //         // const output 
+    //         // console.log(typeof())
+    //         // console.log("glt h")
+    //     }
+    //     output.innerHTML = result
+    // }
 
   
 })
